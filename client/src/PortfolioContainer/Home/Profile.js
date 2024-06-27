ecaffe8cdd833b810bb194a6690f06c88f96f1eb
Starskii <jakeylakie@yahoo.com>
2024-06-26 23:09:20 -0700

    cleaned up profile section

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,63 +1,71 @@
-import React from 'react';
-import { useTypewriter } from 'react-simple-typewriter';
-import './Profile.css'
+import React from "react";
+import { useTypewriter } from "react-simple-typewriter";
+import "./Profile.css";
 
 export default function Profile() {
   const [typeEffect] = useTypewriter({
-    words: ["Software Engineer", "Fullstack Developer", "Machine Learning Engineer", "Robotics Enthusiast"],
+    words: [
+      "Software Engineer",
+      "Fullstack Developer",
+      "Robotics Enthusiast",
+    ],
     loop: {},
     typeSpeed: 80,
     deleteSpeed: 40,
-    delaySpeed: 3000
-  })
+    delaySpeed: 3000,
+  });
   return (
-    <div className='profile-container'>
-      <div className='profile-parent'>
-        <div className='profile-details'>
-          <div className='colz'>
-            <div className='colz-icon'>
-              <a href='https://github.com/Starskii' target="_blank" rel="noopener noreferrer">
-                <i className='fa fa-github-square'></i>
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a
+                href="https://github.com/Starskii"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github-square"></i>
               </a>
-              <a href='https://www.linkedin.com/in/jacob-lakies' target="_blank" rel="noopener noreferrer">
-                <i className='fa fa-linkedin-square'></i>
-              </a>
-            </div>
-          </div>
-          <div className='profile-details-name'>
-            <span className='primary-text'>
-              {" "}
-              Jacob <span className='highlighted-text'>Lakies</span>
-            </span>
-          </div>
-          <div className='profile-details-role'>
-            <span className='primary-text'>
-              {" "}
-              <h1>
-                I'm a {typeEffect} {" "}
-              </h1>
-              <span className='profile-role-tagline'>
-                A software engineer with hands-on experience with robotics, machine learning, and fullstack (MERN) development.
-              </span>
-            </span>
-            <div className='profile-options'>
-              <button className='btn primary-btn'>
-                {" "}
-                Contact Me {" "}
-              </button>
-              <a href='JacobLakiesSoftwareDeveloper.pdf' download='JacobLakiesSoftwareDeveloper.pdf'>
-                <button className='btn highlighted-btn'>Get Resume</button>
+              <a
+                href="https://www.linkedin.com/in/jacob-lakies"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin-square"></i>
               </a>
             </div>
           </div>
-          <div className='profile-picture'>
-            <div className='profile-picture-background'>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Jacob <span className="highlighted-text">Lakies</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>I'm a {typeEffect} </h1>
+            </span>
+            <span className="profile-role-tagline">
+            I strive to build reliable, scalable, and maintainable software solutions.
+            </span>
+          </div>
 
-            </div>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>
+            <a
+              href="JacobLakiesSoftwareDeveloper.pdf"
+              download="JacobLakiesSoftwareDeveloper.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
