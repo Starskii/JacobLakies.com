832e3ee9fb143065ca5c00b20798ebb032157c83
Jacob <jakeylakie@yahoo.com>
2024-07-03 23:07:48 -0700

    started Resume component

---
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './Resume.css';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+
+export default function Resume(props) {
+    const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+    const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({});
+  return (
+    <div> 
+    </div>
+  )
+}
