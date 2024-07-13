38dce5abc9ac395cb2ded14f8f9f2d399c454780
Starskii <jakeylakie@yahoo.com>
2024-07-13 16:44:49 -0700

    updated to add vercel speed insights

--- client/src/PortfolioContainer/Home/Home.js
+++ client/src/PortfolioContainer/Home/Home.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import Profile from './Profile/Profile'
-import Footer from './Footer/Footer'
-import Header from './Header/Header'
-import './Home.css'
+import React from 'react';
+import Profile from './Profile/Profile';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import './Home.css';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
 
-export default function Home() {
+export default function Home(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
   return (
-    <div className='home-container'>
+    <div className='home-container' id={props.id || ''}>
       <Header />
       <Profile /> 
       <Footer />
