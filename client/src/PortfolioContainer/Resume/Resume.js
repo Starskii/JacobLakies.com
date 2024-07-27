d86504e378ec982fc9d528cc4abc77f59abdbc87
Starskii <jakeylakie@yahoo.com>
2024-07-26 22:17:31 -0700

    added resumeBullets and programmingSkillDetails

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -29,7 +29,15 @@ export default function Resume(props) {
                 </div>
             </div>
         </div>;
-    }
+    };
+
+    const resumeBullets = [
+        {label: "Education", logoSrc: "education.svg"},
+        {label: "Work History", logoSrc: "work-history.svg"},
+        {label: "Programming Skills", logoSrc: "programming-skills.svg"},
+        {label: "Projects", logoSrc: "projects.svg"},
+        {label: "Interests", logoSrc: "interests.svg"}
+    ]
 
     let fadeInScreenHandler = (screen) => {
         if (screen.fadeScreen !== props.id) return;
