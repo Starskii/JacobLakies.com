96d1e868edfdb55525b9bc2cd5d571161fd4a850
Starskii <jakeylakie@yahoo.com>
2024-07-30 22:23:04 -0700

    Added ContactMe to the commonUtilities / Header

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import './ContactMe.css'
+import './ContactMe.css';
+import imgBack from '../../assets/ContactMe/mailz.jpeg';
+import loadingBar from '../../assets/ContactMe/load2.gif';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
 
-export default function ContactMe() {
+export default function ContactMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
   return (
     <div>
       
     </div>
   )
 }
+ 
