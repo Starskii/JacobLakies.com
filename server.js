9f4e2e30121420e9c193004228faec119f392ae7
Starskii <jakeylakie@yahoo.com>
2024-08-06 09:46:37 -0700

    added basic server script and fixed dev cmd

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config;
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Creating the middleware 
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Server listening to port 5000`));
