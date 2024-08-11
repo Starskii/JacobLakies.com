688408e36366f1e2cbedd3d6d97930ae44f9fef4
Starskii <jakeylakie@yahoo.com>
2024-08-10 21:19:50 -0700

    redeploy

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ if(process.env.NODE_ENV === "production"){
 }
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server listening to port 5000`));
+app.listen(port, console.log(`Server listening to port 5000`)); 
