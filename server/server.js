6f5ed6b4b42aced7f67759076622027487bfc6ac
Starskii <jakeylakie@yahoo.com>
2024-08-12 14:19:30 -0700

    fixed?

--- server/server.js
+++ server/server.js
@@ -7,17 +7,17 @@ const contactRoute = require('./routes/contactRoute');
 
 const app = express();
 
-// Creating the middleware 
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use("/api/", contactRoute);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => (
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    ))
-}
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server listening to port 5000`)); 
+app.listen(port, console.log(`Server listening to port ${port}`));
