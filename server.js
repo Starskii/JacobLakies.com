7eede0fc06adfa5335a1ca381344901de18952e2
Starskii <jakeylakie@yahoo.com>
2024-08-12 22:35:07 -0700

    removed vercel

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use("/api/", contactRoute);
+app.use("/", contactRoute);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => (
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    ));
+}
 
-app.get("*", (req, res) => {
-    console.log("WOWEE BAUS");
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Server listening to port ${port}`));
+
+module.exports = app;
