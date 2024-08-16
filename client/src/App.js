1ec7d065ad2f4e74b4383e5ff9936851ea10235a
Starskii <jakeylakie@yahoo.com>
2024-08-16 14:08:46 -0700

    added vercel analytics

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import PortfolioContainer from './PortfolioContainer/PortfolioContainer';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/react";
+
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <ToastContainer />
       <PortfolioContainer />
     </div>
